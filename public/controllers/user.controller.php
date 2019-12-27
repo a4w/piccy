1c11cee94caa5f0c16628afc478bd22e011b9732
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2019-12-27 17:08:03 +0200

    Register page controller

---
+++ public/controllers/user.controller.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json;');
+
+use Mapper\UserMapper;
+use Model\User;
+
+$action = $_POST['action'] ?? null;
+
+
+$output = array("error" => false);
+
+switch($action){
+    case 'register':
+        $username = $_POST['username'] ?? null;
+        $password = $_POST['password'] ?? null;
+        $email = $_POST['email'] ?? null;
+        $bio = $_POST['bio'] ?? null;
+        $user = UserMapper::getByUsername($username);
+        if($user !== null){
+            $output['error'] = true;
+            break;
+        }
+        $user = new User(null, $username, $password, $countryID, $email, $bio, null);
+        UserMapper::add($user);
+        break;
+}
+
+echo json_encode($output);
