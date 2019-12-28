ed5e8429a6ffd9f28d623d0894f247e222f99ac1
abduelabied <abduelabied2221@gmail.com>
2019-12-28 13:33:04 +0200

    Fixed some errors in picturereactions and change name of Register

--- public/js/picturereactions.js
+++ public/js/picturereactions.js
@@ -1,26 +1,32 @@
 $(".upvote").click(function(){
 	const id = $(this).attr("data-picture");
-	$.post("controllers/picture.controller.php"),{
+	$.post("controllers/picture.controller.php",{
 			"action":"upvote",
 			"id":$("#id").val()
 	}).done(function(data){});
 });
 $(".downvote").click(function(){
 	const id = $(this).attr("data-picture");
-	$.post("controllers/picture.controller.php"),{
+	$.post("controllers/picture.controller.php",{
 			"action":"downvote",
 			"id":$("#id").val()
 	}).done(function(data){});
 });
 $(".comment").click(function(){
 	const comment = $(this).attr("data-picture");
-	$.post("controllers/picture.controller.php"),{
+	$.post("controllers/picture.controller.php",{
 		"action":"addcomment",
 		"comment":$("comment").val()
-	}).done(function(data){});
+	}).done(function(data){
+
+	});
+});
 $(".follow").click(function(){
 	const id = $(this).attr("data-user");
-	$.post("profile.controller.php"),{
+	$.post("profile.controller.php",{
 	"action":"follow",
-	"id":$("#id").val()}).done(function(data){});
+	"id":$("#id").val()
+	}).done(function(data){
 
+	});
+});
