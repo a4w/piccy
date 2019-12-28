4583041ffa2c3c8f27d7ab5b7cfbc55ab36b3217
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2019-12-28 13:17:56 +0200

    added reactions count to PictureView

--- classes/View/PictureView.php
+++ classes/View/PictureView.php
@@ -4,22 +4,33 @@ namespace View;
 use Mapper\CommentMapper;
 use Mapper\UserMapper;
 use Model\Picture;
+use Mapper\ReactionMapper;
+use Model\Reaction;
+use Model\REACTION_TYPE;
 
 class PictureView extends View{
     const TEMPLATE = 'basic_bs_picture.phtml';
     private $picture;
     private $author;
     private $comments = array();
+    private $downvoteCount = 0, $upvoteCount = 0;
     function __construct(Picture $picture, $template = null){
         $this->picture = $picture;
         $this->author = UserMapper::getByUsername('ahmed');
         $comments = CommentMapper::getAllComments($picture);
+        $reactions = ReactionMapper::getReactionsByPicture($picture);
         foreach($comments as $comment){
             $tmp = array(
                 'comment' => $comment,
                 'user' => UserMapper::getByUsername('ahmed')
             );
             $this->comments[] = $tmp;
+        }
+        foreach($reactions as $reaction){
+            if ($reaction->getType() === REACTION_TYPE::DOWNVOTE)
+                $this->downvoteCount++;
+            else
+                $this->upvoteCount++;
         }
         if($template === null)
             parent::__construct(self::TEMPLATE);
@@ -32,7 +43,9 @@ class PictureView extends View{
         $data = array(
             'author' => $this->author,
             'picture' => $this->picture,
-            'comments' => $this->comments
+            'comments' => $this->comments,
+            'downvoteCount' => $this->downvoteCount,
+            'upvoteCount' => $this->upvoteCount
         );
         return parent::render($data, $returnOnly);
     }
