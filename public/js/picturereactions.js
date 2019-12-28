cf72f294e325a123c46b70ae79abf363947362f5
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2019-12-28 16:22:30 +0200

    Upvotes and downvotes now properly working

--- public/js/picturereactions.js
+++ public/js/picturereactions.js
@@ -1,34 +1,35 @@
 $(".upvote").click(function(){
-	const id = $(this).attr("data-picture");
+	const PictureID = $(this).attr("data-picture");
+	console.log(PictureID);
 	$.post("controllers/picture.controller.php",{
 			"action":"upvote",
-			"id":$("#id").val()
+			"PictureID":PictureID
 	}).done(function(data){});
 });
 $(".downvote").click(function(){
-	const id = $(this).attr("data-picture");
+	const PictureID = $(this).attr("data-picture");
 	$.post("controllers/picture.controller.php",{
 			"action":"downvote",
-			"id":$("#id").val()
+			"PictureID":PictureID
 	}).done(function(data){});
 });
 $(".comment").click(function(){
-	const id = $(this).attr("data-picture");
+	const PictureID = $(this).attr("data-picture");
 	const comment = $(this).parent().parent().find("input");
 	$.post("controllers/picture.controller.php",{
 		"action":"addcomment",
 		"comment":comment.val(),
-		"id":$("#id").val()
+		"PictureID":PictureID
 	}).done(function(data){
 
 	});
 });
-$(".follow").click(function(){
-	const id = $(this).attr("data-user");
-	$.post("profile.controller.php",{
-	"action":"follow",
-	"id":$("#id").val()
-	}).done(function(data){
-
-	});
-});
+// $(".follow").click(function(){
+// 	const PictureID = $(this).attr("data-user");
+// 	$.post("profile.controller.php",{
+// 	"action":"follow",
+// 	"PictureID":PictureID
+// 	}).done(function(data){
+//
+// 	});
+// });
